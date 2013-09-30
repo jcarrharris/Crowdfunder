8af93cbbaddfec19a8204d9e7d3e59145764caf0
cshah3 <shah.control@gmail.com>
2013-09-30 13:25:28 -0400

    disabling fixtures for rspec

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,9 @@ module Crowdfunder
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.generators do |g|
+      g.test_framework :rspec, :fixture => false
+    end
   end
 end
