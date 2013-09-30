43a841fabe16ef848f2f1c0c23ff44487feb1eb2
jcarrharris <joncarrharris@gmail.com>
2013-09-30 14:23:39 -0400

    added guardfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Crowdfunder::Application.routes.draw do
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -11,7 +12,7 @@ Crowdfunder::Application.routes.draw do
   # This route can be invoked with purchase_url(:id => product.id)
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+   #resources :projects
 
   # Sample resource route with options:
   #   resources :products do
---
+++ spec/features/project_flows_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe "Project Listing" do
+  describe "when visiting the index page" do
+    it "should display all projects" do
+      # Create three projects
+      project1 = FactoryGirl.create(:project, :title => "Project 1")
+      project2 = FactoryGirl.create(:project, :title => "Project 2")
+      project3 = FactoryGirl.create(:project, :title => "Project 3")
+
+      # Go to the page with the projects
+      visit "/projects"
+      # Expect the page we're on is the page with the projects
+      expect(current_path).to eq(projects_path)
+
+      # Expect this page has the words 'Listing Projects'
+      page.should have_content('Listing projects')
+      expect(page).to have_content('Listing projects')
+
+      # Expect this page has these words as well
+      page.should have_content('Project 1')
+      expect(page).to have_content('Project 1')
+
+      page.should have_content('Project 2')
+      expect(page).to have_content('Project 2')
+
+      page.should have_content('Project 3')
+      expect(page).to have_content('Project 3')
+    end
+  end
+end
