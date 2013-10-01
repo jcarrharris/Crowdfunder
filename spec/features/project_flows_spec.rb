26ecc5a96c21380ac1cb48795160161be591f556
jcarrharris <joncarrharris@gmail.com>
2013-10-01 18:46:02 -0400

    added pagination

--- spec/features/project_flows_spec.rb
+++ spec/features/project_flows_spec.rb
@@ -70,6 +70,33 @@ describe "Project Listing" do
       page.should have_selector('.navbar ul li.active a', text: "Projects")
       expect(page).to have_selector('.navbar ul li.active a', text: "Projects")
     end
+
+    it "should have pagination" do 
+      user = FactoryGirl.create :user
+      50.times { |i| FactoryGirl.create(:project, title: "Project #{i}", user: user) }
+
+      visit "/projects"
+
+      # Expect the most recently created projects on page 1 (8 PER PAGE)
+      page.should have_content('Project 49')
+      expect(page).to have_content('Project 49')
+
+      page.should have_no_content('Project 41')
+      expect(page).to have_no_content('Project 41')
+
+      page.should have_selector('li.project', count: 8)
+      expect(page).to have_selector('li.project', count: 8)
+
+      # Expect pagination link and click page 2
+      page.find('.pagination').click_link '2'
+
+      # Expect page 2 to have the next 8 projects
+      page.should have_content('Project 41')
+      expect(page).to have_content('Project 41')
+
+      page.should have_no_content('Project 32')
+      expect(page).to have_no_content('Project 32')
+    end
   end
 end
 
