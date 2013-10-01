5fea654040d0a8e2babb6c45daf24e731ad9e504
jcarrharris <joncarrharris@gmail.com>
2013-10-01 10:51:54 -0400

    added support tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -36,4 +36,5 @@ RSpec.configure do |config|
   #     --seed 1234
   config.order = "random"
   config.include Capybara::DSL
+  config.include SessionSupport
 end
---
+++ spec/support/sessions_support.rb
@@ -0,0 +1,16 @@
+module SessionSupport #we can call this helper in any test
+
+	def setup_signed_in_user
+		pass = "this-is-a-password"
+		user = FactoryGirl.create :user, password: pass
+		visit '/session/new'
+
+		fill_in "email", with: user.email
+		fill_in "password", with: password
+		click_button "Login"
+		# Return our user when this method is called
+		user
+
+		# No expectations are written
+	end
+end
